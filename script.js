0a61ecd559f95b8de0993629cef6a9a012e1a04b
Han Wong <han.wong@pm.me>
2020-12-12 20:18:15 +0100

    added new pokemon array and encrypting letters

--- script.js
+++ script.js
@@ -1,15 +1,42 @@
 const body = getElement('body');
-const words = ["bulbasaur", "ivysaur", "venusaur", "charmander", "charmeleon", "charizard", "squirtle", "wartortle", "blastoise", "caterpie", "metapod", "butterfree", "weedle", "kakuna", "beedrill", "pidgey", "pidgeotto", "pidgeot", "rattata", "raticate", "spearow", "fearow", "ekans", "arbok", "pikachu", "raichu", "sandshrew", "sandslash", "nidoran", "nidorina", "nidoqueen", "nidoran", "nidorino", "nidoking", "clefairy", "clefable", "vulpix", "ninetales", "jigglypuff", "wigglytuff", "zubat", "golbat", "oddish", "gloom", "vileplume", "paras", "parasect", "venonat", "venomoth", "diglett", "dugtrio", "meowth", "persian", "psyduck", "golduck", "mankey", "primeape", "growlithe", "arcanine", "poliwag", "poliwhirl", "poliwrath", "abra", "kadabra", "alakazam", "machop", "machoke", "machamp", "bellsprout", "weepinbell", "victreebel", "tentacool", "tentacruel", "geodude", "graveler", "golem", "ponyta", "rapidash", "slowpoke", "slowbro", "magnemite", "magneton", "farfetchd", "doduo", "dodrio", "seel", "dewgong", "grimer", "muk", "shellder", "cloyster", "gastly", "haunter", "gengar", "onix", "drowzee", "hypno", "krabby", "kingler", "voltorb", "electrode", "exeggcute", "exeggutor", "cubone", "marowak", "hitmonlee", "hitmonchan", "lickitung", "koffing", "weezing", "rhyhorn", "rhydon", "chansey", "tangela", "kangaskhan", "horsea", "seadra", "goldeen", "seaking", "staryu", "starmie", "mrmime", "scyther", "jynx", "electabuzz", "magmar", "pinsir", "tauros", "magikarp", "gyarados", "lapras", "ditto", "eevee", "vaporeon", "jolteon", "flareon", "porygon", "omanyte", "omastar", "kabuto", "kabutops", "aerodactyl", "snorlax", "articuno", "zapdos", "moltres", "dratini", "dragonair", "dragonite", "mewtwo", "mew", "chikorita", "bayleef", "meganium", "cyndaquil", "quilava", "typhlosion", "totodile", "croconaw", "feraligatr", "sentret", "furret", "hoothoot", "noctowl", "ledyba", "ledian", "spinarak", "ariados", "crobat", "chinchou", "lanturn", "pichu", "cleffa", "igglybuff", "togepi", "togetic", "natu", "xatu", "mareep", "flaaffy", "ampharos", "bellossom", "marill", "azumarill", "sudowoodo", "politoed", "hoppip", "skiploom", "jumpluff", "aipom", "sunkern", "sunflora", "yanma", "wooper", "quagsire", "espeon", "umbreon", "murkrow", "slowking", "misdreavus", "unown", "wobbuffet", "girafarig", "pineco", "forretress", "dunsparce", "gligar", "steelix", "snubbull", "granbull", "qwilfish", "scizor", "shuckle", "heracross", "sneasel", "teddiursa", "ursaring", "slugma", "magcargo", "swinub", "piloswine", "corsola", "remoraid", "octillery", "delibird", "mantine", "skarmory", "houndour", "houndoom", "kingdra", "phanpy", "donphan", "porygon2", "stantler", "smeargle", "tyrogue", "hitmontop", "smoochum", "elekid", "magby", "miltank", "blissey", "raikou", "entei", "suicune", "larvitar", "pupitar", "tyranitar", "lugia", "ho-oh", "celebi", "treecko", "grovyle", "sceptile", "torchic", "combusken", "blaziken", "mudkip", "marshtomp", "swampert", "poochyena", "mightyena", "zigzagoon", "linoone", "wurmple", "silcoon", "beautifly", "cascoon", "dustox", "lotad", "lombre", "ludicolo", "seedot", "nuzleaf", "shiftry", "taillow", "swellow", "wingull", "pelipper", "ralts", "kirlia", "gardevoir", "surskit", "masquerain", "shroomish", "breloom", "slakoth", "vigoroth", "slaking", "nincada", "ninjask", "shedinja", "whismur", "loudred", "exploud", "makuhita", "hariyama", "azurill", "nosepass", "skitty", "delcatty", "sableye", "mawile", "aron", "lairon", "aggron", "meditite", "medicham", "electrike", "manectric", "plusle", "minun", "volbeat", "illumise", "roselia", "gulpin", "swalot", "carvanha", "sharpedo", "wailmer", "wailord", "numel", "camerupt", "torkoal", "spoink", "grumpig", "spinda", "trapinch", "vibrava", "flygon", "cacnea", "cacturne", "swablu", "altaria", "zangoose", "seviper", "lunatone", "solrock", "barboach", "whiscash", "corphish", "crawdaunt", "baltoy", "claydol", "lileep", "cradily", "anorith", "armaldo", "feebas", "milotic", "castform", "kecleon", "shuppet", "banette", "duskull", "dusclops", "tropius", "chimecho", "absol", "wynaut", "snorunt", "glalie", "spheal", "sealeo", "walrein", "clamperl", "huntail", "gorebyss", "relicanth", "luvdisc", "bagon", "shelgon", "salamence", "beldum", "metang", "metagross", "regirock", "regice", "registeel", "latias", "latios", "kyogre", "groudon", "rayquaza", "jirachi", "deoxys", "turtwig", "grotle", "torterra", "chimchar", "monferno", "infernape", "piplup", "prinplup", "empoleon", "starly", "staravia", "staraptor", "bidoof", "bibarel", "kricketot", "kricketune", "shinx", "luxio", "luxray", "budew", "roserade", "cranidos", "rampardos", "shieldon", "bastiodon", "burmy", "wormadam", "mothim", "combee", "vespiquen", "pachirisu", "buizel", "floatzel", "cherubi", "cherrim", "shellos", "gastrodon", "ambipom", "drifloon", "drifblim", "buneary", "lopunny", "mismagius", "honchkrow", "glameow", "purugly", "chingling", "stunky", "skuntank", "bronzor", "bronzong", "bonsly", "mime jr.", "happiny", "chatot", "spiritomb", "gible", "gabite", "garchomp", "munchlax", "riolu", "lucario", "hippopotas", "hippowdon", "skorupi", "drapion", "croagunk", "toxicroak", "carnivine", "finneon", "lumineon", "mantyke", "snover", "abomasnow", "weavile", "magnezone", "lickilicky", "rhyperior", "tangrowth", "electivire", "magmortar", "togekiss", "yanmega", "leafeon", "glaceon", "gliscor", "mamoswine", "porygon-z", "gallade", "probopass", "dusknoir", "froslass", "rotom", "uxie", "mesprit", "azelf", "dialga", "palkia", "heatran", "regigigas", "giratina", "cresselia", "phione", "manaphy", "darkrai", "shaymin", "arceus", "victini", "snivy", "servine", "serperior", "tepig", "pignite", "emboar", "oshawott", "dewott", "samurott", "patrat", "watchog", "lillipup", "herdier", "stoutland", "purrloin", "liepard", "pansage", "simisage", "pansear", "simisear", "panpour", "simipour", "munna", "musharna", "pidove", "tranquill", "unfezant", "blitzle", "zebstrika", "roggenrola", "boldore", "gigalith", "woobat", "swoobat", "drilbur", "excadrill", "audino", "timburr", "gurdurr", "conkeldurr", "tympole", "palpitoad", "seismitoad", "throh", "sawk", "sewaddle", "swadloon", "leavanny", "venipede", "whirlipede", "scolipede", "cottonee", "whimsicott", "petilil", "lilligant", "basculin", "sandile", "krokorok", "krookodile", "darumaka", "darmanitan", "maractus", "dwebble", "crustle", "scraggy", "scrafty", "sigilyph", "yamask", "cofagrigus", "tirtouga", "carracosta", "archen", "archeops", "trubbish", "garbodor", "zorua", "zoroark", "minccino", "cinccino", "gothita", "gothorita", "gothitelle", "solosis", "duosion", "reuniclus", "ducklett", "swanna", "vanillite", "vanillish", "vanilluxe", "deerling", "sawsbuck", "emolga", "karrablast", "escavalier", "foongus", "amoonguss", "frillish", "jellicent", "alomomola", "joltik", "galvantula", "ferroseed", "ferrothorn", "klink", "klang", "klinklang", "tynamo", "eelektrik", "eelektross", "elgyem", "beheeyem", "litwick", "lampent", "chandelure", "axew", "fraxure", "haxorus", "cubchoo", "beartic", "cryogonal", "shelmet", "accelgor", "stunfisk", "mienfoo", "mienshao", "druddigon", "golett", "golurk", "pawniard", "bisharp", "bouffalant", "rufflet", "braviary", "vullaby", "mandibuzz", "heatmor", "durant", "deino", "zweilous", "hydreigon", "larvesta", "volcarona", "cobalion", "terrakion", "virizion", "tornadus", "thundurus", "reshiram", "zekrom", "landorus", "kyurem", "keldeo", "meloetta", "genesect", "chespin", "quilladin", "chesnaught", "fennekin", "braixen", "delphox", "froakie", "frogadier", "greninja", "bunnelby", "diggersby", "fletchling", "fletchinder", "talonflame", "scatterbug", "spewpa", "vivillon", "litleo", "pyroar", "flabebe", "floette", "florges", "skiddo", "gogoat", "pancham", "pangoro", "furfrou", "espurr", "meowstic", "honedge", "doublade", "aegislash", "spritzee", "aromatisse", "swirlix", "slurpuff", "inkay", "malamar", "binacle", "barbaracle", "skrelp", "dragalge", "clauncher", "clawitzer", "helioptile", "heliolisk", "tyrunt", "tyrantrum", "amaura", "aurorus", "sylveon", "hawlucha", "dedenne", "carbink", "goomy", "sliggoo", "goodra", "klefki", "phantump", "trevenant", "pumpkaboo", "gourgeist", "bergmite", "avalugg", "noibat", "noivern", "xerneas", "yveltal", "zygarde", "diancie", "hoopa", "volcanion", "rowlet", "dartrix", "decidueye", "litten", "torracat", "incineroar", "popplio", "brionne", "primarina", "pikipek", "trumbeak", "toucannon", "yungoos", "gumshoos", "grubbin", "charjabug", "vikavolt", "crabrawler", "crabominable", "oricorio", "cutiefly", "ribombee", "rockruff", "lycanroc", "wishiwashi", "mareanie", "toxapex", "mudbray", "mudsdale", "dewpider", "araquanid", "fomantis", "lurantis", "morelull", "shiinotic", "salandit", "salazzle", "stufful", "bewear", "bounsweet", "steenee", "tsareena", "comfey", "oranguru", "passimian", "wimpod", "golisopod", "sandygast", "palossand", "pyukumuku", "silvally", "minior", "komala", "turtonator", "togedemaru", "mimikyu", "bruxish", "drampa", "dhelmise", "jangmo-o", "hakamo-o", "kommo-o", "tapu koko", "tapu lele", "tapu bulu", "tapu fini", "cosmog", "cosmoem", "solgaleo", "lunala", "nihilego", "buzzwole", "pheromosa", "xurkitree", "celesteela", "kartana", "guzzlord", "necrozma", "magearna", "marshadow", "poipole", "naganadel", "stakataka", "blacephalon", "zeraora", "meltan", "melmetal"];
+let words = null;
 const qwerty = 'qwertyuiopasdfghjklzxcvbnm';
+const special = ':.- ’♀♂';
+const hidden = '';
 const main = getElement('main');
 let word;
 const pMessage = getElement('.message');
 const ulWord = getElement('.word');
 const ulQwerty = getElement('.qwerty');
 const ulScore = getElements('.score>li');
+const url = './pokemons.json';
+let key;
+let pokemon;
 let score = 0;
 let numberOfGuesses = 10;
-newGame();
+
+document.onload = init(url);
+
+function export2txt() {
+    // let pokemons = [];
+    // for (let i = 0; i < words.length; i++) {
+    //     const element = words[i];
+    //     let pokemon = {};
+    //     pokemon['pokedex'] = '#' + (i + 1);
+    //     pokemon['name'] = element;
+    //     pokemons.push(pokemon);
+
+    // }
+    // console.log(pokemons);
+    const a = document.createElement("a");
+    a.href = URL.createObjectURL(new Blob([JSON.stringify(pokemons2, null, 2)], {
+        type: "text/plain"
+    }));
+    a.setAttribute("download", "data.txt");
+    document.body.appendChild(a);
+    a.click();
+    document.body.removeChild(a);
+}
 
 function clearMessage() {
     pMessage.textContent = '';
@@ -49,6 +76,14 @@ function displayScore(score) {
     ulScore[0].textContent = `Score: ${score}`;
     ulScore[1].textContent = `Guesses left: ${numberOfGuesses}`;
 }
+function encrypt(char) {
+    let i = 0;
+    for (i; i < qwerty.length; i++) {
+        if (qwerty[i] == char)
+            break;
+    }
+    return ((key + i) % qwerty.length).toString();
+}
 
 function getElement(selector) {
     return document.querySelector(selector);
@@ -59,13 +94,17 @@ function getElements(selector) {
 }
 
 function handleGuess(key) {
-    let letterbox = getElements('.letterbox');
-    if (word.includes(key)) {
-        for (let i = 0; i < word.length; i++) {
-            if (word[i] == key) letterbox[i].textContent = key;
+    let letterboxes = getElements('.letterbox');
+    let encryptedLetters = [];
+    for (const letterbox of letterboxes) {
+        encryptedLetters.push(letterbox.dataset.letter);
+    }
+    if (encryptedLetters.includes(encrypt(key))) {
+        for (let i = 0; i < encryptedLetters.length; i++) {
+            if (encryptedLetters[i] == encrypt(key)) letterboxes[i].textContent = key;
         }
         displayMessage('match');
-        score += Math.floor(20 * numberOfGuesses * (1 + 1 / word.length));
+        score += 10;
         displayScore(score);
     } else {
         displayMessage('no-match');
@@ -74,7 +113,7 @@ function handleGuess(key) {
         displayScore(score);
     }
     let guess = '';
-    for (const letter of letterbox) {
+    for (const letter of letterboxes) {
         guess += letter.textContent;
     }
 
@@ -91,11 +130,11 @@ function handleClick(e) {
     let keyboard = getElements('.letters');
     for (const key of keyboard) {
         if (key.dataset.letter == e.target.dataset.letter) {
-            if (key.className.includes('disabled')) {
+            if (key.className.includes('invisible')) {
                 displayMessage('same-key-pressed');
             } else {
                 displayMessage('you-guessed', e.target.dataset.letter[3]);
-                key.classList.add('disabled');
+                key.classList.add('invisible');
                 handleGuess(e.target.dataset.letter[3].toLowerCase());
             }
         }
@@ -105,22 +144,41 @@ function handleKeypress(e) {
     let keyboard = getElements('.letters');
     for (const key of keyboard) {
         if (key.dataset.letter == e.code) {
-            if (key.className.includes('disabled')) {
+            if (key.className.includes('invisible')) {
                 displayMessage('same-key-pressed');
             } else {
                 displayMessage('you-guessed', e.code[3]);
-                key.classList.add('disabled');
+                key.classList.add('invisible');
                 handleGuess(e.code[3].toLowerCase());
             }
         }
     }
 }
 
-function newGame() {
-    word = randomWord();
-    for (let i = 0; i < word.length; i++) {
-        const li = create('li', 'letterbox', null, 'letter', i);
-        ulWord.append(li);
+function init(url) {
+    fetch(url)
+        .then(response => response.json())
+        .then(data => newGame(data))
+        .catch(err => console.log(err));
+}
+function newGame(pokemons) {
+    let pokemon = randomPokemon(pokemons);
+    console.log(pokemon);
+    getElement('.pokemon').src = pokemon.src;
+    let name = pokemon.name.toLowerCase();
+    for (let i = 0; i < name.length; i++) {
+        if (qwerty.includes(name[i])) {
+            const li = create('li', 'letterbox', null, 'letter', encrypt(name[i]));
+            ulWord.append(li);
+        } else if (special.includes(name[i])) {
+            const li = create('li', 'letterbox', encrypt(name[i]));
+            li.classList.add('special');
+            ulWord.append(li);
+        } else {
+            const li = create('li', 'letterbox', encrypt(name[i]));
+            li.classList.add('hidden');
+            ulWord.append(li);
+        }
     }
     for (let i = 0; i < qwerty.length; i++) {
         const li = create('li', 'letters', qwerty[i], 'letter', `Key${qwerty[i].toUpperCase()}`);
@@ -130,7 +188,10 @@ function newGame() {
     body.addEventListener('click', handleClick);
 }
 
-function randomWord() { return words[Math.floor(Math.random() * words.length)]; };
+function randomPokemon(words) {
+    key = Math.floor(Math.random() * words.length);
+    return words[key];
+};
 
 
 
